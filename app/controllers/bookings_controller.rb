d2052b9cb0643489a7a9669b9b620e1f38aef58e
Beryl Armand <beryl.armand@gmail.com>
2021-03-16 14:56:06 +0100

    Data collect explained

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -14,6 +14,20 @@ def show
 
 end
 
+
+def search
+  raise
+  @departure = params[:departure]
+  @start_date = params[:start_date]
+  @nights = params[:nights]
+  @place = params[:place]
+  @rythm = params[:rythm]
+  @passengers = params[:passengers]
+  @van_type = params[:van_type]
+  # récupérer le van en fonction de passengers et van type
+  van = Van.find_from_criteria(@passengers, @van_type)
+end
+
 private
 
   def booking_params
--- app/models/van.rb
+++ app/models/van.rb
@@ -1,7 +1,10 @@
 class Van < ApplicationRecord
 
   has_many :bookings
-
   validates :name, :passengers, :van_type, :price_per_night, presence: true
+
+  def self.find_from_criteria(passengers, van_type)
+    all.find(passa)
+  end
 
 end
