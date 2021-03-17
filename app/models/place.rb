74363e55e9e2d9e23fc6a48aa8997ce730c65445
tanguy leveque <leveque.tanguy@outlook.fr>
2021-03-17 15:12:21 +0100

    geocode ok

--- app/models/place.rb
+++ app/models/place.rb
@@ -5,4 +5,7 @@ class Place < ApplicationRecord
 
   validates :name, :address, :sleeping_type, :description, presence: true
 
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_17_113726) do
+ActiveRecord::Schema.define(version: 2021_03_17_135552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,8 @@ ActiveRecord::Schema.define(version: 2021_03_17_113726) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
