2c94d6555f368dd763c9552ee9d56501a618638d
SophieCrenet <sophie.crenet@gmail.com>
2021-03-17 12:42:40 +0100

    seeds ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_17_104240) do
+ActiveRecord::Schema.define(version: 2021_03_17_113726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,6 +103,7 @@ ActiveRecord::Schema.define(version: 2021_03_17_104240) do
     t.integer "price_per_night"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
