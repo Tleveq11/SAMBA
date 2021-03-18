48c6eae756e73688d8d648e5f0c69eb4f7542b0e
SophieCrenet <sophie.crenet@gmail.com>
2021-03-18 14:44:23 +0100

    seeds-place working ok

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,6 @@
 class Place < ApplicationRecord
 
-  belongs_to :booking_places
+  has_many :booking_places
   has_one_attached :photo
 
   validates :name, :address, :sleeping_type, :description, presence: true
